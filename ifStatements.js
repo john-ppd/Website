cdba5bd79d46042a98166c21be303a015fc070dd
john-ppd <63131902+john-ppd@users.noreply.github.com>
2020-04-22 17:34:37 -0400

    more changes

---
+++ ifStatements.js
@@ -0,0 +1,12 @@
+
+
+document.getElementById("p1").style.backgroundColor = "blue";
+
+document.getElementById("b1").onclick = function () {
+  var x = document.getElementById("in1").value;
+
+  if (x == "hi") {
+    alert("hii");
+  }
+
+}
