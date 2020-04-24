c0d3354d90c1d8f1fcb3ae47d3c016f1d92f10ab
john-ppd <63131902+john-ppd@users.noreply.github.com>
2020-04-24 16:09:58 -0400

    more timer code

--- countDownTimer/countDownTimer.js
+++ countDownTimer/countDownTimer.js
@@ -1,7 +1,7 @@
 var x = 0;
 function setTime(time) {
-  x = time;
-  document.getElementById("timeBox").innerHTML = time;
+  x = time * 60;
+  document.getElementById("timeBox").innerHTML = x;
 
 }
 
@@ -11,25 +11,26 @@ document.getElementById("min5").onclick = function () { setTime(5) };
 
 document.getElementById("min15").onclick = function () { setTime(15) };
 
-var timeStart = Date.now();
-var timerStartValue;
-function getStartTime() {
-  console.log("timeStart is: " + timeStart);
-
-  timerStartValue = timeStart + (x * 60000);
-
-  console.log("timeStartTime is: " + timerStartValue);
-
-  console.log("x in function is: " + x);
+function countDown() {
+  x--;
+  document.getElementById("timeBox").innerHTML = x;
 }
 
+var timerRun;
+var timeOn = false;
 document.getElementById("buttonGo").onclick = function () {
-  getStartTime();
-  document.getElementById("timeBox").innerHTML = timerStartValue;
 
-  console.log("x value is " + x);
+  if (timeOn == false) {
+    timerRun = setInterval(countDown, 1000);
+    timeOn = true;
+    document.getElementById("buttonGo").innerHTML = "STOP";
+    document.getElementById("buttonGo").style.backgroundColor = "red";
+  }
+  else if (timeOn == true) {
+    clearInterval(timerRun);
+    document.getElementById("buttonGo").innerHTML = "START";
+    document.getElementById("buttonGo").style.backgroundColor = "green";
+    timeOn = false;
+  }
 
-  document.getElementById("timeBox2").innerHTML = timeStart;
-
-}
-
+};
